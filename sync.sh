fb3367524f11573c8e2a489fc589e93953113092
Shah Sawood <shahsawoodshinwari@gmail.com>
2025-03-08 21:08:14 +0500

    added command to create 404 file for gh

--- sync.sh
+++ sync.sh
@@ -2,3 +2,5 @@
 
 # Sync files from ../frontend-tms/dist/ to the current directory, excluding .git and sync.sh
 rsync -av --delete --exclude='.git' --exclude='sync.sh' ../frontend-tms/dist/ .
+
+cp index.html 404.html
